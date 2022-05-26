7a5e83165b8c916b5c76c4071f551d911ebc3c61
HongJungKim(이든) <58525009+HongJungKim-dev@users.noreply.github.com>
2022-05-26 16:28:05 +0900

    [Feat] 모달 구현 (#8)
    * docs: 템플릿 수정(리뷰반영)
    
    * refactor: config폴더내의 파일을 루트로 이동(리뷰반영)
    
    * build: react-refresh 추가
    
    * feat: 모달 구현

--- FE/webpack.common.js
+++ FE/webpack.common.js
@@ -2,23 +2,33 @@ const path = require('path');
 
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const createStyledComponentsTransformer = require('typescript-plugin-styled-components').default;
 const webpack = require('webpack');
+const styledComponentsTransformer = createStyledComponentsTransformer();
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '../src'),
+      '@': path.resolve(__dirname, 'src'),
     },
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
   },
-  entry: `${path.resolve(__dirname, '../src')}/index.tsx`,
+  entry: `${path.resolve(__dirname, 'src')}/index.tsx`,
   module: {
-    rules: [],
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          getCustomTransformers: () => ({ before: [styledComponentsTransformer] }),
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: `${path.resolve(__dirname, '../public')}/index.html`,
+      template: `${path.resolve(__dirname, 'public')}/index.html`,
     }),
     new webpack.ProvidePlugin({ React: 'react' }),
     new ReactRefreshWebpackPlugin(),
