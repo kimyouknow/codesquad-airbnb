2f6b9558ade6fdb08d9c47655a8531f20ced3adf
khj <hongjung.kim.dev@gmail.com>
2022-06-02 15:08:18 +0900

    feat: 달력 무한 캐러셀 우측 구현

---
+++ FE/src/components/CalendarModal/style.js
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import styled from 'styled-components';
+
+export const CalendarContainer = styled.div`
+  width: 440px;
+  height: 440px;
+`;
+
+export const Wrapper = styled.div`
+  overflow: hidden;
+`;
+
+export const ItemContainer = styled.ul`
+  display: flex;
+  width: 100%;
+  padding: 0; // TODO : reset css 적용시 삭제하기
+  transition: ${({ transtion }) => transtion};
+  transform: ${({ nextCount, divide }) =>
+    nextCount === 0 ? `translateX(-50%)` : `translateX(${-50 * (nextCount % divide)}%)`};
+`;
+
+export const Item = styled.li`
+  flex-shrink: 0;
+  width: ${({ itemGap }) => `calc(50% - ${itemGap}px)`};
+  margin: 0 ${({ itemGap }) => itemGap / 2}px;
+  list-style: none; // TODO : reset css 적용시 삭제하기
+`;
