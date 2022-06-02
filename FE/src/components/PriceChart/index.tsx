08247a2b9929c2c3835b57d1c5bd5036408a1158
yunho <kimyouknow@naver.com>
2022-06-02 10:48:03 +0900

    Feat: PriceChart 범용성 있게 변수 변경
    - 데이터는 선언할 때 넘겨주기
    - x,y축 범용성있게 네이밍 변경

--- FE/src/components/PriceChart/index.tsx
+++ FE/src/components/PriceChart/index.tsx
@@ -1,21 +1,40 @@
-import { useEffect, useLayoutEffect, useRef } from 'react';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 
 import { CANVAS_WIDTH, CANVAS_HEIGHT } from '@/components/PriceChart/constants';
 
 import * as S from './style';
 
 interface RoomCapacityType {
+  [index: string]: number;
   range: number;
   count: number;
 }
 
+interface PriceChartProps {
+  chartInfo: RoomCapacityType[];
+  axis: { x: string; y: string };
+}
+
 const START_X = 0;
 
-export default function PriceChart() {
+export default function PriceChart({ chartInfo, axis }: PriceChartProps) {
+  const xDataset = chartInfo.map(element => element[axis.x]);
+  const yDataset = chartInfo.map(element => element[axis.y]);
+  const maximumX = Math.max(...xDataset);
+  const maximumY = Math.max(...yDataset);
+
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [minXThumb, setMinXThumb] = useState(0);
+  const [maxXThumb, setMaxXThumb] = useState(maximumX);
+
+  const handleMaxPriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event;
+    setMaxXThumb(Number(value));
+  };
 
   const renderCanvas = () => {
-    // canvas 자체
     if (!canvasRef.current) {
       return;
     }
@@ -24,8 +43,7 @@ export default function PriceChart() {
     canvas.height = CANVAS_HEIGHT;
   };
 
-  const drawLine = () => {
-    // 선그리는 로직
+  const drawBackgroundChart = () => {
     if (!canvasRef.current) {
       return;
     }
@@ -34,77 +52,79 @@ export default function PriceChart() {
     const context = canvas.getContext('2d') as CanvasRenderingContext2D;
 
     context.beginPath();
-    context.moveTo(START_X, CANVAS_HEIGHT); // 시작점으로 이동
+    context.moveTo(START_X, CANVAS_HEIGHT); // 시작점(좌하단)으로 이동
 
-    setPositions(context, data);
+    setPositions(context, xDataset, yDataset, maximumX, maximumY);
 
-    context.stroke(); // 선 그리기
-    fillContext(context);
+    context.stroke();
+    fillContext(context, 0, CANVAS_WIDTH, 'wheat');
+  };
+
+  const drawAciveChart = (leftX: number, rigthX: number) => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const canvas: HTMLCanvasElement = canvasRef.current;
+    const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+    context.strokeStyle = 'darkGreen';
+    context.lineWidth = 4;
+
+    const newChartInfo = chartInfo.filter(({ range }) => range < rigthX);
+    console.log('newChartInfo :>> ', newChartInfo, rigthX);
+    // setPositions(context, newChartInfo, maxRange, maxCount);
+    // context.stroke();
+
+    // fillContext(context, leftX, rigthX, 'tomato');
   };
 
   useLayoutEffect(() => {
     renderCanvas();
+    drawBackgroundChart();
   }, []);
 
   useEffect(() => {
-    drawLine();
-  }, []);
+    drawAciveChart(minXThumb, maxXThumb);
+  }, [minXThumb, maxXThumb]);
 
   return (
     <S.CanvasContainer>
       <S.Canvas ref={canvasRef}></S.Canvas>
+      <input
+        type="range"
+        value={maxXThumb}
+        step={10}
+        min={0}
+        max={maximumX}
+        onChange={handleMaxPriceChange}
+      />
     </S.CanvasContainer>
   );
 }
 
-const setPositions = (context: CanvasRenderingContext2D, roomCapacities: RoomCapacityType[]) => {
-  const maxRange = Math.max(...roomCapacities.map(({ range }) => range));
-  const maxCount = Math.max(...roomCapacities.map(({ count }) => count));
-
-  roomCapacities.forEach(({ range, count }) => {
-    const x = (range / maxRange) * CANVAS_WIDTH;
-    const y = CANVAS_HEIGHT - (count / maxCount) * CANVAS_HEIGHT;
+const setPositions = (
+  context: CanvasRenderingContext2D,
+  xDataset: number[],
+  yDataset: number[],
+  maximumX: number,
+  maximumY: number,
+) => {
+  xDataset.forEach((rawX, index) => {
+    const rawY = yDataset[index];
+    const x = (rawX / maximumX) * CANVAS_WIDTH;
+    const y = CANVAS_HEIGHT - (rawY / maximumY) * CANVAS_HEIGHT;
     context.lineTo(x, y);
   });
 };
 
-const fillContext = (context: CanvasRenderingContext2D) => {
-  context.lineTo(CANVAS_WIDTH, CANVAS_HEIGHT); // 좌표 맨 오른쪽 끝
-  context.lineTo(0, CANVAS_HEIGHT); // 좌표 시작으로 다시 가기
-  context.fillStyle = 'rgb(93, 92, 92)';
-  context.globalCompositeOperation = 'destination-over'; // draw behind border를 색칠한 부분 뒤로 보냄
-  context.fill(); // will close the path for us (Every time this method is called, the list is reset and we can start drawing new shapes.)
+const fillContext = (
+  context: CanvasRenderingContext2D,
+  leftXPosition: number,
+  rigthXPosition: number,
+  bgColor: string,
+) => {
+  context.lineTo(rigthXPosition, CANVAS_HEIGHT); // 좌표 맨 오른쪽 끝
+  context.lineTo(leftXPosition, CANVAS_HEIGHT); // 좌표 시작으로 다시 가기
+  context.fillStyle = bgColor;
+  context.fill();
 };
-
-const data = [
-  { range: 10000, count: 33 },
-  { range: 20000, count: 31 },
-  { range: 30000, count: 33 },
-  { range: 40000, count: 45 },
-  { range: 50000, count: 31 },
-  { range: 60000, count: 43 },
-  { range: 70000, count: 70 },
-  { range: 80000, count: 43 },
-  { range: 90000, count: 37 },
-  { range: 100000, count: 32 },
-  { range: 110000, count: 35 },
-  { range: 120000, count: 45 },
-  { range: 130000, count: 21 },
-  { range: 140000, count: 54 },
-  { range: 150000, count: 71 },
-  { range: 160000, count: 35 },
-  { range: 170000, count: 16 },
-  { range: 180000, count: 13 },
-  { range: 190000, count: 4 },
-  { range: 200000, count: 18 },
-  { range: 210000, count: 54 },
-  { range: 220000, count: 66 },
-  { range: 230000, count: 31 },
-  { range: 240000, count: 37 },
-  { range: 250000, count: 9 },
-  { range: 260000, count: 41 },
-  { range: 270000, count: 76 },
-  { range: 280000, count: 47 },
-  { range: 290000, count: 64 },
-  { range: 300000, count: 69 },
-];
