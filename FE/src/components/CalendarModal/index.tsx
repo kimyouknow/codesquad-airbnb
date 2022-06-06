f8617223f255b11f5075b768ae6760a97ebc8e85
khj <hongjung.kim.dev@gmail.com>
2022-06-06 20:56:34 +0900

    refactor: transitionEnd를 활용하여 캘린더 캐러셀 로직 리팩토링, 캘린더 보이는 개수 변수화하여 자동화

--- FE/src/components/CalendarModal/index.tsx
+++ FE/src/components/CalendarModal/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import Calendar from '@/components/Calendar';
 
@@ -15,52 +15,38 @@ export default function CalendarModal({ isModalOpen, handleOpenModal }: Calendar
   const today = new Date();
   const year = today.getFullYear();
   const month = today.getMonth();
+
   const [activeYear, setActiveYear] = useState(year);
   const [activeMonth, setActiveMonth] = useState(month);
-  // TODO : 12월 넘어가면 1월부터 다시 시작하고 year은 1추가로 수정하기
-  const [months, setMonths] = useState([
-    activeMonth - 1,
-    activeMonth,
-    activeMonth + 1,
-    activeMonth + 2,
-    activeMonth + 3,
-  ]);
-  const [nextCount, setNextCount] = useState(1);
-  const [isCombackSlide, setIsCombackSlide] = useState(false);
-  const [transition, setTransition] = useState('transform 1s linear 0s');
 
-  const divide = months.length - 1;
-  const currentMonthOrder = 1;
-  const lastMonthOrder = divide - 1;
-  const increasedMonth = divide - 2;
+  const showingCardNum = 2;
+  const HIDDEN_CARD_NUM = 2;
+  const initialMonths = Array.from(
+    { length: showingCardNum + HIDDEN_CARD_NUM },
+    (_, index) => index - 1 + activeMonth,
+  );
+  // TODO : 12월 넘어가면 1월부터 다시 시작하고 year은 1추가로 수정하기
+  const [months, setMonths] = useState(initialMonths);
+  const [slideXCount, setSlideXCount] = useState(1);
+  const [isRightSliding, setIsRightSliding] = useState(false);
+  const INCREASED_MONTH = 1;
+  const INCREASED_SLIDE_X_COUNT = 1;
+  const INITIAL_SLIDE_X_COUNT = 1;
   const itemGap = 26;
 
   const handleClickPreviousCalendar = () => {};
 
   const handleClickNextCalendar = () => {
-    setActiveMonth(activeMonth + 1);
-    const newOrder = (nextCount + 1) % divide;
-    const firstMonthOrder = newOrder + 1;
-    setNextCount(nextCount === lastMonthOrder ? firstMonthOrder : newOrder);
+    setIsRightSliding(true);
+    setSlideXCount(slideXCount + INCREASED_SLIDE_X_COUNT);
   };
 
-  useEffect(() => {
-    // FIXME: useEffect 내부 로직 함수로 분리
-    if (nextCount === currentMonthOrder) {
-      setTransition('transform 1s linear 0s');
-      if (isCombackSlide) {
-        // TODO : 12월 넘어가면 1월부터 다시 시작하고 year은 1추가로 수정하기
-        setMonths([...months.map(currentMonth => currentMonth + increasedMonth)]);
-        setTimeout(function () {
-          setNextCount((nextCount + 1) % divide);
-        }, 0);
-        setTransition('');
-      }
-      setIsCombackSlide(false);
-    } else if (nextCount === lastMonthOrder) {
-      setIsCombackSlide(true);
-    }
-  }, [nextCount, transition, isCombackSlide]);
+  const handleTransitionEnd = () => {
+    setActiveMonth(activeMonth + INCREASED_MONTH);
+    setSlideXCount(INITIAL_SLIDE_X_COUNT);
+    setMonths(months.map(currentMonth => currentMonth + INCREASED_MONTH));
+    setIsRightSliding(false);
+  };
 
   return (
     <WindowModal show={isModalOpen} handleOpenModal={handleOpenModal}>
@@ -70,9 +56,18 @@ export default function CalendarModal({ isModalOpen, handleOpenModal }: Calendar
           이전달
         </button>
         <S.Wrapper>
-          <S.ItemContainer nextCount={nextCount} transition={transition}>
+          <S.ItemContainer
+            slideXCount={slideXCount}
+            isRightSliding={isRightSliding}
+            onTransitionEnd={handleTransitionEnd}
+            showingCardNum={showingCardNum}
+          >
             {months.map(currentActiveMonth => (
-              <S.Item key={`activeMonth-${currentActiveMonth}`} itemGap={itemGap}>
+              <S.Item
+                key={`activeMonth-${currentActiveMonth}`}
+                itemGap={itemGap}
+                showingCardNum={showingCardNum}
+              >
                 <Calendar activeMonth={currentActiveMonth} activeYear={activeYear} />
               </S.Item>
             ))}
