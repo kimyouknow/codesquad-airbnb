044520ecfe535ce63d85e2b34af6140b046fff88
YUNHO <kimyouknow@naver.com>
2022-06-07 21:26:45 +0900

    [Refactor] 캘린더 좌측 클릭시 렌더링 및 리팩토링  (#34)
    * Refactor: 초기렌더링 에러 수정 및 years,months 데이터 통합
    
    getNextMonthsNyears() -> getMonthsWithYear()
    
    years, month -> calendarHeaderDate
    
    * Feat: 좌측 버튼 캐러셀 로직 구현
    
    * Refactor: 달을 기준으로 년도를 생성하는 함수 분리 - getMonthsWithYear()
    
    * Refactor: 좌우로직을 하나의 상태와 함수로 관리
    
    * Refactor: Calendar모달에서 캐러셀 분리
    
    * Refactor: 초기값 부모컴포넌트로 위임
    
    * Chore: styled-component에 ts적용

--- FE/src/components/CalendarCourousel/style.ts
+++ FE/src/components/CalendarCourousel/style.ts
@@ -12,16 +12,23 @@ export const Wrapper = styled.div`
   overflow: hidden;
 `;
 
-export const ItemContainer = styled.ul`
+export const ItemContainer = styled.ul<{
+  canTransition: boolean;
+  slideXCount: number;
+  showingCardNum: number;
+}>`
   display: flex;
   width: 100%;
   padding: 0; // TODO : reset css 적용시 삭제하기
-  transition: ${({ isRightSliding }) => isRightSliding && 'transform 1s linear 0s'};
+  transition: ${({ canTransition }) => canTransition && 'transform 0.5s linear 0s'};
   transform: ${({ slideXCount, showingCardNum }) =>
     `translateX(${-(100 / showingCardNum) * slideXCount}%)`};
 `;
 
-export const Item = styled.li`
+export const Item = styled.li<{
+  itemGap: number;
+  showingCardNum: number;
+}>`
   flex-shrink: 0;
   width: ${({ itemGap, showingCardNum }) => `calc(${100 / showingCardNum}% - ${itemGap}px)`};
   margin: 0 ${({ itemGap }) => itemGap / 2}px;
