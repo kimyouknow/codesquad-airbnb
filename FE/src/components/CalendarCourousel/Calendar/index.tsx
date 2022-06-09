9489ea192d118c312a0be62a9d9ffd54d7f4a1b8
HongJungKim(이든) <58525009+HongJungKim-dev@users.noreply.github.com>
2022-06-10 01:08:12 +0900

    [Feat] 캘린더, date picker 스타일 적용 및 리팩토링 (#42)
    * Feat: date picker mouseover, click시 스타일 변경 구현
    
    * Feat: 체크인을 설정하고 다른 달력으로 이동시 mouseover 스타일이 적용되지 않도록구현
    
    * Feat: 한 달 내의 이전달 혹은 다음 달 범위의 날짜 스타일 및 이벤트 비활성화
    
    * Fix: 달력 범위 벗어나면 클릭하여 설정한 체크아웃까지의 범위 스타일 해제되는 오류 해결
    
    * Feat: 년도와 여러 달을 지나쳐 체크아웃을 설정해도 체크인 체크아웃 사이의 모든 날짜가 스타일 적용되도록 구현

--- FE/src/components/CalendarCourousel/Calendar/index.tsx
+++ FE/src/components/CalendarCourousel/Calendar/index.tsx
@@ -4,21 +4,26 @@ import { months, daysOfWeek, saturdayNumber } from '@/constants/constants';
 import * as S from './style';
 
 interface CalendarInfoType {
-  activeYear: number;
-  activeMonth: number;
-}
-
-interface CalendarProps {
-  activeYear: number;
-  activeMonth: number;
-  handleClickDay: (selectedDate: SelectedDateType) => void;
-}
-
-interface DateProps {
   year: number;
   month: number;
-  isAciveMonth: boolean;
+}
+
+export interface FullDateProps {
+  year: number;
+  month: number;
   date: number;
+}
+
+interface CalendarProps extends CalendarInfoType {
+  handleClickDay: (selectedDate: SelectedDateType) => void;
+  handleMouseOverDay: (selectedDate: SelectedDateType) => void;
+  handleMouseLeaveCalendar: () => void;
+  checkIn: FullDateProps;
+  checkOut: FullDateProps;
+}
+
+interface DateProps extends FullDateProps {
+  isAciveMonth: boolean;
 }
 
 interface CalendarDateProps extends DateProps {
@@ -33,21 +38,71 @@ interface DatesProps {
   addedDate: number;
 }
 
-export default function Calendar({ activeYear, activeMonth, handleClickDay }: CalendarProps) {
-  const dates = getCalendarInfo({ activeYear, activeMonth });
+export default function Calendar({
+  year,
+  month,
+  handleClickDay,
+  handleMouseOverDay,
+  handleMouseLeaveCalendar,
+  checkIn,
+  checkOut,
+}: CalendarProps) {
+  const dates = getCalendarInfo({ year, month });
+
+  // TODO : 로직은 최하단으로 이동하기
+  const checkIsSelectedDatePoint = (date: number) => {
+    const isEqualWithCheckIn =
+      checkIn.year === year && checkIn.month === month && checkIn.date === date;
+    const isEqualWithCheckOut =
+      checkOut.year === year && checkOut.month === month && checkOut.date === date;
+
+    return isEqualWithCheckIn || isEqualWithCheckOut;
+  };
+
+  const checkIsSelectedDateRange = (date: number) => {
+    const checkInConvertedNum = date2convertedNum({
+      year: checkIn.year,
+      month: checkIn.month,
+      date: checkIn.date,
+    });
+
+    const currentDateConvertedNum = date2convertedNum({
+      year,
+      month,
+      date,
+    });
+
+    const checkOutConvertedNum = date2convertedNum({
+      year: checkOut.year,
+      month: checkOut.month,
+      date: checkOut.date,
+    });
+
+    return (
+      checkInConvertedNum < currentDateConvertedNum &&
+      currentDateConvertedNum < checkOutConvertedNum
+    );
+  };
+
   return (
     <S.Container>
-      <S.CalendarTitle>{`${months[activeMonth]} ${activeYear}`}</S.CalendarTitle>
-      <S.WeekContainer>
+      <S.CalendarTitle>{`${months[month]} ${year}`}</S.CalendarTitle>
+      <S.WeekDayItemContainer>
         {daysOfWeek.map(day => (
-          <S.DayItem key={`day-${day}`}>{day}</S.DayItem>
+          <S.WeekDayItem key={`day-${day}`}>{day}</S.WeekDayItem>
         ))}
-      </S.WeekContainer>
-      <S.WeekContainer>
-        {dates.map(({ id, date }) => (
+      </S.WeekDayItemContainer>
+      <S.WeekContainer onMouseLeave={handleMouseLeaveCalendar}>
+        {dates.map(({ id, date, isAciveMonth }) => (
           <S.DayItem
             key={id}
-            onClick={() => handleClickDay({ year: activeYear, month: activeMonth, date })}
+            onClick={() => handleClickDay({ year, month, date })}
+            onMouseOver={() => handleMouseOverDay({ year, month, date })}
+            checkIn={checkIn}
+            checkOut={checkOut}
+            isSelectedDatePoint={checkIsSelectedDatePoint(date)}
+            isSelectedDateRange={checkIsSelectedDateRange(date)}
+            isAciveMonth={isAciveMonth}
           >
             {date}
           </S.DayItem>
@@ -57,31 +112,26 @@ export default function Calendar({ activeYear, activeMonth, handleClickDay }: Ca
   );
 }
 
-const getCalendarInfo = ({ activeYear, activeMonth }: CalendarInfoType): CalendarDateProps[] => {
-  const prevMonthLastFullDate = new Date(activeYear, activeMonth, 0);
+const getCalendarInfo = ({ year, month }: CalendarInfoType): CalendarDateProps[] => {
+  const prevMonthLastFullDate = new Date(year, month, 0);
   const prevMonthLastDay = prevMonthLastFullDate.getDay();
   const prevMonthLastDate = prevMonthLastFullDate.getDate();
 
-  const activeMonthLastFullDate = new Date(activeYear, activeMonth + 1, 0);
+  const activeMonthLastFullDate = new Date(year, month + 1, 0);
   const activeMonthLastDay = activeMonthLastFullDate.getDay();
   const activeMonthLastDate = activeMonthLastFullDate.getDate();
 
-  const prevMonthDates = getPrevMonthLastWeek(
-    prevMonthLastDay,
-    prevMonthLastDate,
-    activeYear,
-    activeMonth - 1,
-  );
+  const prevMonthDates = getPrevMonthLastWeek(prevMonthLastDay, prevMonthLastDate, year, month - 1);
 
   const currentMonthDates = getDates({
     length: activeMonthLastDate,
-    year: activeYear,
-    month: activeMonth,
+    year,
+    month,
     addedDate: 0,
     isAciveMonth: true,
   });
 
-  const nextMonthDates = getNextMonthFirstWeek(activeMonthLastDay, activeYear, activeMonth + 1);
+  const nextMonthDates = getNextMonthFirstWeek(activeMonthLastDay, year, month + 1);
 
   return prevMonthDates.concat(currentMonthDates, nextMonthDates);
 };
@@ -142,3 +192,18 @@ const getDate = ({ year, month, date, isAciveMonth }: DateProps): CalendarDatePr
 });
 
 const isSaturDay = (numberOfDay: number) => numberOfDay !== saturdayNumber;
+
+const date2convertedNum = ({ year, month, date }: FullDateProps) => {
+  const ONE_YEAR_DAY = 365;
+  const fromJanToCurrentMonth = Array.from({ length: month }, (_, index) => index);
+  const totalDatesFromJanToPreviousMonth = fromJanToCurrentMonth
+    .map(currentMonth => new Date(year, currentMonth, 0).getDate())
+    .reduce((accumulatedDates, currentDates) => accumulatedDates + currentDates, 0);
+
+  /**
+   * 2022.5.8이라고 하면
+   * 2022 * 365 + 1, 2 3, 4월달의 모든일(31, 28, 30, 29) + 8일 -> 738,156
+   */
+
+  return year * ONE_YEAR_DAY + totalDatesFromJanToPreviousMonth + date;
+};
