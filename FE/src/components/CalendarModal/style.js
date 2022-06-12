c0fee5e6daddddde08a36e111c5b160303e7abd7
yunho <kimyouknow@naver.com>
2022-06-13 08:50:16 +0900

    [Refactor] 캘린더 우측 클릭시 렌더링 리팩토링 (#30)
    * refactor: transitionEnd를 활용하여 캘린더 캐러셀 로직 리팩토링, 캘린더 보이는 개수 변수화하여 자동화
    
    * feat: 다음 달력 조회시 년도 넘어가면 년도 최신화, 캐러셀 버튼 디바운스 적용

---
+++ FE/src/components/CalendarModal/style.js
@@ -0,0 +1,29 @@
+/* eslint-disable prettier/prettier */
+import styled from 'styled-components';
+
+export const CalendarContainer = styled.div`
+  width: 440px;
+  height: 440px;
+  display: 'flex';
+  gap: '12px';
+`;
+
+export const Wrapper = styled.div`
+  overflow: hidden;
+`;
+
+export const ItemContainer = styled.ul`
+  display: flex;
+  width: 100%;
+  padding: 0; // TODO : reset css 적용시 삭제하기
+  transition: ${({ isRightSliding }) => isRightSliding && 'transform 1s linear 0s'};
+  transform: ${({ slideXCount, showingCardNum }) =>
+    `translateX(${-(100 / showingCardNum) * slideXCount}%)`};
+`;
+
+export const Item = styled.li`
+  flex-shrink: 0;
+  width: ${({ itemGap, showingCardNum }) => `calc(${100 / showingCardNum}% - ${itemGap}px)`};
+  margin: 0 ${({ itemGap }) => itemGap / 2}px;
+  list-style: none; // TODO : reset css 적용시 삭제하기
+`;
